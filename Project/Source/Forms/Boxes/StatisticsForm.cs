b0865ce88516cf47d8f25216551e73dad0090b67
Olivier Rogier <github.com@ordisoftware.org>
2019-09-23 21:50:14 +0200

    Refactor

--- Project/Source/Forms/Boxes/StatisticsForm.cs
+++ Project/Source/Forms/Boxes/StatisticsForm.cs
@@ -119,7 +119,7 @@ namespace Ordisoftware.HebrewWords
 
     private void InitializeMiddle()
     {
-      int lcount = (int)Math.Truncate((double)CountersTorah.CountLetters / 2.0);
+      int lcount = (int)Math.Truncate(CountersTorah.CountLetters / 2.0);
       int index = 0;
       var books = from book in MainForm.Instance.DataSet.Books
                   where book.Number <= BooksBounds.Torah.Max
