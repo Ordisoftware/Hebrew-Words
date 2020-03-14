efd41f36c60deddd9bfdb39a06ac2568e3c45584
Olivier Rogier <github.com@ordisoftware.org>
2020-03-14 08:11:32 +0100

    Update letters tool

--- Project/Source/Classes/HebrewLetters.cs
+++ Project/Source/Classes/HebrewLetters.cs
@@ -1,6 +1,6 @@
 ﻿/// <license>
 /// This file is part of Ordisoftware Hebrew Words.
-/// Copyright 2012-2019 Olivier Rogier.
+/// Copyright 2012-2020 Olivier Rogier.
 /// See www.ordisoftware.com for more information.
 /// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 /// If a copy of the MPL was not distributed with this file, You can obtain one at 
@@ -11,7 +11,7 @@
 /// You may add additional accurate notices of copyright ownership.
 /// </license>
 /// <created> 2012-10 </created>
-/// <edited> 2019-09 </edited>
+/// <edited> 2020-03 </edited>
 using System;
 using System.Collections.Generic;
 
@@ -84,14 +84,34 @@ namespace Ordisoftware.HebrewWords
     static public string ConvertToHebrewFont(string str)
     {
       string result = "";
-      foreach ( char c in str ) result += ConvertToKey(c);
+      foreach ( char c in str )
+      {
+        char cNew = ConvertToKey(c);
+        if ( cNew != '\0' )
+          result += cNew;
+      }
+      return result;
+    }
+
+    /// <summary>
+    // Convert hebrew font key codes to standard hebrew letters.
+    /// </summary>
+    static public string ConvertToUnicode(string str)
+    {
+      string result = "";
+      foreach ( char c in str )
+      {
+        char cNew = ConvertToUnicode(c);
+        if ( cNew != '\0' )
+          result = cNew + result;
+      }
       return result;
     }
 
     /// <summary>
     // Convert letters from www.fourmilab.ch/etexts/www/hebrew/Bible to font codes.
     /// </summary>
-    static private char ConvertToKey(char c)
+    static public char ConvertToKey(char c)
     {
       switch ( c )
       {
@@ -124,9 +144,93 @@ namespace Ordisoftware.HebrewWords
         case 'ת': return 't';
         case ':': return '.';
         case '-': return ' ';
-        default: return c;
+        default: return '\0';
       }
     }
+
+    static public char ConvertToUnicode(char c)
+    {
+      switch ( c )
+      {
+        case 'a': return 'א';
+        case 'b': return 'ב';
+        case 'g': return 'ג';
+        case 'd': return 'ד';
+        case 'h': return 'ה';
+        case 'v': return 'ו';
+        case 'z': return 'ז';
+        case 'x': return 'ח';
+        case 'u': return 'ט';
+        case 'y': return 'י';
+        case 'k': return 'כ';
+        case '!': return 'ך';
+        case 'l': return 'ל';
+        case 'm': return 'מ';
+        case ',': return 'ם';
+        case 'n': return 'נ';
+        case ']': return 'ן';
+        case 'c': return 'ס';
+        case 'i': return 'ע';
+        case 'p': return 'פ';
+        case '[': return 'ף';
+        case 'j': return 'צ';
+        case '/': return 'ץ';
+        case 'q': return 'ק';
+        case 'r': return 'ר';
+        case '>': return 'ש';
+        case 't': return 'ת';
+        case '.': return ':';
+        case ' ': return '-';
+        default: return '\0';
+      }
+    }
+
+    /// <summary>
+    /// Indicate letters simple values.
+    /// </summary>
+    static public readonly int[] ValuesSimple =
+    {
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20,
+      30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400
+    };
+
+    /// <summary>
+    /// Indicate letters simple values.
+    /// </summary>
+    static public readonly int[] ValuesFull =
+    {
+      111, 412, 83, 434, 6, 12, 67, 418, 419, 20, 100,
+      74, 90, 106, 120, 130, 81, 104, 186, 510, 360, 406
+    };
+
+    /// <summary>
+    /// Indicate letters names.
+    /// </summary>
+    static public readonly Dictionary<string, string[]> Names
+      = new Dictionary<string, string[]>()
+      {
+        {
+          "en", new string[]
+          { "Alef", "Bet", "Gimel", "Dalet", "He", "Vav", "Zayin", "'Het", "Tet", "Yod", "Kaf",
+            "Lamed", "Mem", "Nun", "Samek", "'Ayin", "Pay", "Tsadi", "Qof", "Resh", "Shin", "Tav"
+          }
+        },
+        {
+          "fr", new string[]
+          { "Alef", "Bet", "Guimel", "Dalet", "Hé", "Vav", "Zayin", "'Het", "Tet", "Youd", "Kaf",
+            "Lamed", "Mem", "Noun", "Samek", "'Ayin", "Pé", "Tsadi", "Qouf", "Resh", "Shin", "Tav"
+          }
+        }
+      };
+
+    /// <summary>
+    /// Indicate hebrew letters names.
+    /// </summary>
+    static public readonly string[] HebrewNames =
+    {
+      "pla", "tyb", "lmyg", "tld", "ah", "vv", "]yz", "tx", "tu", "dvy", "pk",
+      "dml", ",m", "]vn", "!mc", "]yi", "hp", "ydj", "pvq", ">r", "]y>", "vt",
+    };
 
   }
 
