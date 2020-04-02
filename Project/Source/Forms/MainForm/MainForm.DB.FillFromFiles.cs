d035d7b5d7fd8d7045deda7db0bc666beb4e7eea
Olivier Rogier <github.com@ordisoftware.org>
2020-04-02 11:13:07 +0200

    Add new empty db fields

--- Project/Source/Forms/MainForm/MainForm.DB.FillFromFiles.cs
+++ Project/Source/Forms/MainForm/MainForm.DB.FillFromFiles.cs
@@ -11,7 +11,7 @@
 /// You may add additional accurate notices of copyright ownership.
 /// </license>
 /// <created> 2019-01 </created>
-/// <edited> 2019-01 </edited>
+/// <edited> 2020-03 </edited>
 using System;
 using System.Data;
 using System.IO;
