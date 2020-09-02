3823bb81d9f6cd927731e7e58f6c2fd244cbe8ca
Olivier Rogier <github.com@ordisoftware.org>
2020-09-02 16:54:37 +0200

    Add log management

--- Project/Source/Program/Program.cs
+++ Project/Source/Program/Program.cs
@@ -50,7 +50,7 @@ namespace Ordisoftware.HebrewWords
       Application.SetCompatibleTextRenderingDefault(false);
       Globals.Settings = Settings;
       Globals.MainForm = MainForm.Instance;
-      ExceptionManager.Active = Settings.DebuggerEnabled;
+      DebugManager.Active = Settings.DebuggerEnabled;
       string lang = Settings.Language;
       Shell.CheckCommandLineArguments(args, ref lang);
       Settings.Language = lang;
