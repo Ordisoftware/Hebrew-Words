3af3a70da1526b9ac701234f7b7ab169c475090a
Olivier Rogier <github.com@ordisoftware.org>
2020-09-02 22:44:54 +0200

    Improve UI/UX

--- Project/Source/Program/Program.cs
+++ Project/Source/Program/Program.cs
@@ -50,7 +50,7 @@ namespace Ordisoftware.HebrewWords
       Application.SetCompatibleTextRenderingDefault(false);
       Globals.Settings = Settings;
       Globals.MainForm = MainForm.Instance;
-      DebugManager.Active = Settings.DebuggerEnabled;
+      DebugManager.Enabled = Settings.DebuggerEnabled;
       string lang = Settings.Language;
       Shell.CheckCommandLineArguments(args, ref lang);
       Settings.Language = lang;
