cc3888cd40557ad4fa5356be39c4fc1f8ff10da6
Olivier Rogier <github.com@ordisoftware.org>
2020-12-12 09:11:02 +0100

    Change to CommandLineParser dependency

--- Project/Source/Program/Program.cs
+++ Project/Source/Program/Program.cs
@@ -56,7 +56,7 @@ namespace Ordisoftware.Hebrew.Words
       DebugManager.Enabled = Settings.DebuggerEnabled;
       DebugManager.TraceEnabled = true;// Settings.TraceEnabled;
       Language lang = Settings.LanguageSelected;
-      SystemManager.CheckCommandLineArguments(args, ref lang);
+      SystemManager.CheckCommandLineArguments<SystemCommandLineArgs>(args, ref lang);
       Settings.LanguageSelected = lang;
       UpdateLocalization();
       Application.Run(MainForm.Instance);
