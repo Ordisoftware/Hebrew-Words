57054161586792c306a8230fc1241f35cb279471
Olivier Rogier <github.com@ordisoftware.org>
2021-03-09 11:18:38 +0100

    Fix code

--- Project/Source/Program/Program.cs
+++ Project/Source/Program/Program.cs
@@ -50,7 +50,7 @@ namespace Ordisoftware.Hebrew.Words
         Globals.IsSettingsUpgraded = upgrade;
         Settings.CheckUpgradeRequired(ref upgrade);
         Settings.UpgradeRequired = upgrade;
-        Globals.IsSettingsUpgraded = Globals.IsSettingsUpgraded && !Settings.FirstLaunch;
+        if ( Globals.IsSettingsUpgraded ) Settings.FirstLaunch = false;
         CheckSettingsReset();
         if ( lang != Language.None ) Settings.LanguageSelected = lang;
         Settings.Save();
