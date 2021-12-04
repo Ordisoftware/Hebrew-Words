6fcc8fdf0b1d8dc88f6f6d73baed2e42c92e6d85
Olivier Rogier <github.com@ordisoftware.org>
2021-12-04 23:17:15 +0100

    Improve code

--- Project/Source/Forms/MainForm/MainForm.cs
+++ Project/Source/Forms/MainForm/MainForm.cs
@@ -425,6 +425,7 @@ partial class MainForm : Form
   /// <param name="e">Event information.</param>
   private void ActionViewVerses_Click(object sender, EventArgs e)
   {
+    // TODO move all view clicks in setview
     ActionSave.PerformClick();
     if ( Settings.CurrentView == ViewMode.Verses ) return;
     SetView(ViewMode.Verses);
