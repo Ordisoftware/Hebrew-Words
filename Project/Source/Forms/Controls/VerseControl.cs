9b0b6d40d9fedbe389c7f03628254c8cbb6528ec
Olivier Rogier <github.com@ordisoftware.org>
2021-12-09 15:37:33 +0100

    Fix UI

--- Project/Source/Forms/Controls/VerseControl.cs
+++ Project/Source/Forms/Controls/VerseControl.cs
@@ -17,16 +17,23 @@ namespace Ordisoftware.Hebrew.Words;
 public partial class VerseControl : UserControl
 {
 
+  private class MetricsItem
+  {
+    public int ControlWidth;
+    public int EditCommentaryTextHeight;
+    public int EditCommentaryHeight;
+    public int LabelVerseNumberWidth;
+    public int EditCommentaryMarginLeft;
+    public int WordControlsPerLine;
+    public Font LabelVerseNumberFont;
+    public Font EditCommentaryFont;
+  }
+
   static private readonly Properties.Settings Settings = Program.Settings;
 
-  static internal bool ResetMetricsRequired = true;
-  static int EditCommentaryTextHeight;
-  static int EditCommentaryHeight;
-  static int LabelVerseNumberWidth;
-  static int EditCommentaryMarginLeft;
-  static int WordControlsPerLine;
-  static Font LabelVerseNumberFont;
-  static Font EditCommentaryFont;
+  static private readonly Dictionary<Panel, MetricsItem> MetricsCollection = new();
+
+  static internal bool ResetMetricsRequired;
 
   public WordControl[] WordControls { get; init; }
 
@@ -41,44 +48,62 @@ public partial class VerseControl : UserControl
   {
     Reference = reference;
     WordControls = new WordControl[reference.Verse.Words.Count];
-    Width = container.ClientSize.Width;
-    if ( ResetMetricsRequired ) ResetMetrics(container);   // TODO update ResetTextHeight after win resized & maximized
+    MetricsItem metrics;
+    if ( !MetricsCollection.ContainsKey(container) )
+    {
+      metrics = new MetricsItem();
+      MetricsCollection.Add(container, metrics);
+      ResetMetrics(container);
+    }
+    else
+      metrics = MetricsCollection[container];
+    if ( ResetMetricsRequired ) ResetMetrics();
     if ( Settings.VerseCommentaryLinesCount > 1 )
     {
       EditCommentary.Multiline = true;
       EditCommentary.WordWrap = true;
       EditCommentary.ScrollBars = ScrollBars.Vertical;
-      PanelComment.Height = EditCommentaryHeight;
+      PanelComment.Height = metrics.EditCommentaryHeight;
     }
-    int numberOfLines = WordControls.Length / WordControlsPerLine;
-    if ( WordControls.Length % WordControlsPerLine > 0 ) numberOfLines++;
-    LabelVerseNumber.Font = LabelVerseNumberFont;
+    int numberOfLines = WordControls.Length / metrics.WordControlsPerLine;
+    if ( WordControls.Length % metrics.WordControlsPerLine > 0 ) numberOfLines++;
+    LabelVerseNumber.Font = metrics.LabelVerseNumberFont;
     LabelVerseNumber.Text = reference.Verse.Number.ToString();
-    LabelVerseNumber.Width = LabelVerseNumberWidth;
+    LabelVerseNumber.Width = metrics.LabelVerseNumberWidth;
     LabelVerseNumber.ContextMenuStrip = MainForm.Instance.ContextMenuStripVerse;
-    EditCommentary.Font = EditCommentaryFont;
+    EditCommentary.Font = metrics.EditCommentaryFont;
     EditCommentary.Text = reference.Verse.Comment;
     EditCommentary.DataBindings.Add("Text", reference.Verse, "Comment", false, DataSourceUpdateMode.OnPropertyChanged);
-    PanelCommentLeft.Width = EditCommentaryMarginLeft;
+    PanelCommentLeft.Width = metrics.EditCommentaryMarginLeft;
     Height = Padding.Top + Padding.Left + Padding.Bottom + CreateWordControls() * numberOfLines + EditCommentary.Height;
+    Width = metrics.ControlWidth;
+  }
+
+  public void ResetMetrics()
+  {
+    foreach ( var container in MetricsCollection.Keys )
+      ResetMetrics(container);
   }
 
   public void ResetMetrics(Panel container)
   {
+    var metrics = MetricsCollection[container];
     using var graphicsNumber = LabelVerseNumber.CreateGraphics();
     using var graphicsCommentary = EditCommentary.CreateGraphics();
-    LabelVerseNumberWidth = TextRenderer.MeasureText(graphicsNumber, "000", LabelVerseNumber.Font).Width + 10;
-    int width = container.ClientSize.Width + new VScrollBar().Width
-              - container.Padding.Left - container.Padding.Right
+    metrics.LabelVerseNumberWidth = TextRenderer.MeasureText(graphicsNumber, "000", LabelVerseNumber.Font).Width + 10;
+    int widthHScroll = new VScrollBar().Width;
+    int delta = 5 + container.Padding.Left + container.Padding.Right;
+    int width = container.ClientSize.Width
+              - delta * 2
               - Padding.Left - Padding.Right
-              - LabelVerseNumberWidth;
-    WordControlsPerLine = width / Settings.WordControlWidth;
-    EditCommentaryTextHeight = TextRenderer.MeasureText(graphicsCommentary, "A", EditCommentary.Font).Height;
-    EditCommentaryHeight = EditCommentaryTextHeight * ( Settings.VerseCommentaryLinesCount + 1 ) - 5;
-    EditCommentaryMarginLeft = width + Padding.Left - Settings.WordControlWidth * WordControlsPerLine;
-    LabelVerseNumberFont = new Font(LabelVerseNumber.Font.FontFamily, Settings.FontSizeHebrew - 2, FontStyle.Bold);
-    EditCommentaryFont = new Font(EditCommentary.Font.FontFamily, Settings.FontSizeCommentary);
-    ResetMetricsRequired = false;
+              - metrics.LabelVerseNumberWidth;
+    metrics.ControlWidth = container.ClientSize.Width - widthHScroll - delta;
+    metrics.WordControlsPerLine = width / Settings.WordControlWidth;
+    metrics.EditCommentaryTextHeight = TextRenderer.MeasureText(graphicsCommentary, "A", EditCommentary.Font).Height;
+    metrics.EditCommentaryHeight = metrics.EditCommentaryTextHeight * ( Settings.VerseCommentaryLinesCount + 1 ) - 5;
+    metrics.EditCommentaryMarginLeft = width + Padding.Left - Settings.WordControlWidth * metrics.WordControlsPerLine;
+    metrics.LabelVerseNumberFont = new Font(LabelVerseNumber.Font.FontFamily, Settings.FontSizeHebrew - 2, FontStyle.Bold);
+    metrics.EditCommentaryFont = new Font(EditCommentary.Font.FontFamily, Settings.FontSizeCommentary);
   }
 
   private int CreateWordControls()
