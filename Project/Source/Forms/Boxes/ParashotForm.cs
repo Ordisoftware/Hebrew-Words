a36b398623510d41d9839a1fe96eaffd8f6f00c0
Olivier Rogier <github.com@ordisoftware.org>
2022-09-24 17:23:27 +0200

    Update file

--- Project/Source/Forms/Boxes/ParashotForm.cs
+++ Project/Source/Forms/Boxes/ParashotForm.cs
@@ -346,7 +346,7 @@ partial class ParashotForm : Form
     e.ThrowException = false;
   }
 
-  private KeysConverter KeysConverter = new KeysConverter();
+  private KeysConverter KeysConverter = new();
 
   private void DataGridView_KeyDown(object sender, KeyEventArgs e)
   {
